2ac666bb55be69d7ce10e1facb3d6fd841a032b0
Chris <itzchwis@gmail.com>
2026-02-28 15:54:39 -0600

    Update Models.swift

--- App/Sources/Models.swift
+++ App/Sources/Models.swift
@@ -1,14 +1,14 @@
 import Foundation
 
 struct DomainRule: Identifiable, Codable {
-    let id = UUID()
+    var id = UUID()
     var pattern: String
     var isBlocked: Bool
     var isActive: Bool
 }
 
 struct DNSLog: Identifiable, Codable {
-    let id = UUID()
+    var id = UUID()
     let timestamp: Date
     let domain: String
     let blocked: Bool
